80d7abd49d3c8058e0ed1c0d064ebc54db9e0443
Born2BCoder <born2bcoder@icloud.com>
2020-02-29 21:03:20 -0300

    Upload Proyect

---
+++ Toon Water/Assets/Scripts/Perlin_Water.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perlin_Water : MonoBehaviour
+{
+
+    private Perlin Perlin_Noise;
+
+    public float Distorcion;
+    public float Velocidad;
+
+    private Vector3[] Base;
+    
+    void Start()
+    {
+        Perlin_Noise = new Perlin();
+    }
+
+    
+    void Update()
+    {
+        Mesh _mesh = GetComponent<MeshFilter>().mesh;
+
+        if(Base == null)
+        {
+            Base = new Vector3[_mesh.vertexCount];
+            Base = _mesh.vertices;
+        }
+
+        Vector3[] Vertices = _mesh.vertices;
+
+        float timeX = Time.time * Velocidad;
+        float timeY = Time.time * Velocidad;
+        float timeZ = Time.time * Velocidad;
+
+        for(int i = 0; i < Vertices.Length; i++)
+        {
+            Vector3 Auxiliar = Base[i];
+
+            Auxiliar.x += Perlin_Noise.Noise(timeX + Auxiliar.x, timeX + Auxiliar.y, timeX + Auxiliar.z) * Distorcion;
+            Auxiliar.y += Perlin_Noise.Noise(timeY + Auxiliar.x, timeY + Auxiliar.y, timeY + Auxiliar.z) * Distorcion;
+            Auxiliar.z += Perlin_Noise.Noise(timeZ + Auxiliar.x, timeZ + Auxiliar.y, timeZ + Auxiliar.z) * Distorcion;
+
+            Vertices[i] = Auxiliar;
+
+        }
+
+        _mesh.vertices = Vertices;
+        _mesh.RecalculateNormals();
+
+    }
+}
---
+++ Toon Water/Assets/Scripts/Texutre_Scroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texutre_Scroller : MonoBehaviour
+{
+
+    private Material mr;
+
+    public float ScrollingSpeed = 0;
+    private Vector2 ScrollVector = new Vector2(0, 0);
+
+    public bool Scroll_X;
+    public bool Scroll_Y;
+    
+    void Start()
+    {
+        mr = GetComponent<Renderer>().material;
+    }
+
+    
+    void Update()
+    {
+        float offset = Time.time * ScrollingSpeed;
+
+        if (Scroll_X) { ScrollVector.x = offset; }
+        if (Scroll_Y) { ScrollVector.y = offset; }
+
+        if(Scroll_X || Scroll_Y)
+        {
+            mr.mainTextureOffset = ScrollVector;
+        }
+    }
+}
